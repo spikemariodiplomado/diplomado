141e3a3a1f7998afea3de67f531063450d42ba8f
Victor <mena.victor@gmail.com>
2019-11-16 17:03:55 -0500

    Implementación de direccionamiento desde lista hacia detalle

--- p2pfiles/src/app/app-routing.module.ts
+++ p2pfiles/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ListadoEmpresasComponent } from './components/admin-empresa/listado-empresas/listado-empresas.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { FormEmpresaComponent } from './components/admin-empresa/form-empresa/form-empresa.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'empresas', component: ListadoEmpresasComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'empresas', component: ListadoEmpresasComponent },
+  { path: 'empresa', component: FormEmpresaComponent },
+  { path: 'empresa/:id', component: FormEmpresaComponent },
   { path: '**', component: LoginComponent }
 ];
 
--- p2pfiles/src/app/components/admin-empresa/listado-empresas/listado-empresas.component.ts
+++ p2pfiles/src/app/components/admin-empresa/listado-empresas/listado-empresas.component.ts
@@ -1,34 +1,53 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Renderer, AfterViewInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { EmpresasService } from 'src/app/services/empresas.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listado-empresas',
   templateUrl: './listado-empresas.component.html',
   styleUrls: ['./listado-empresas.component.css']
 })
-export class ListadoEmpresasComponent implements OnInit {
+export class ListadoEmpresasComponent implements OnInit, AfterViewInit {
 
   dtOptions: any = {};
 
-  constructor(private http: Http, private empresaSrv: EmpresasService) { }
+  constructor(private http: Http, private empresaSrv: EmpresasService, private renderer: Renderer, private router: Router) { }
 
   ngOnInit() {
     this.dtOptions = {
       ajax: '/assets/data/data.json',
-      columns: [{
-        title: 'ID',
-        data: 'id'
-      }, {
-        title: 'First name',
-        data: 'firstName'
-      }, {
-        title: 'Last name',
-        data: 'lastName'
-      }],
+      columns: [
+        {
+          title: 'ID',
+          data: 'id'
+        }, {
+          title: 'First name',
+          data: 'firstName'
+        }, {
+          title: 'Last name',
+          data: 'lastName'
+        }, {
+          data: 'id',
+          title: 'Action',
+          render: function (data: any, type: any, full: any) {
+            // return '<button>Ver Detalle</button>';
+            return '<a href="/empresa/'+data+'"><span>Detalle</span></a>'
+          }
+        }
+      ],
       dom: 'Bfrtip',
-      buttons: ['excel']
+      buttons: ['excel'],
+      select: true
     };     
+  }
+
+  ngAfterViewInit(): void {
+    this.renderer.listenGlobal('document', 'click', (event) => {
+      if (event.target.hasAttribute("view-person-id")) {
+        this.router.navigate(["/person/" + event.target.getAttribute("view-person-id")]);
+      }
+    });
   }
 
   ngOnDestroy(): void {
