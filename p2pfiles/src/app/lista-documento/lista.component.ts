bc93b4e644bc922687dc64fa394dab6de156dbd3
Marieofpillar9 <57118108+Marieofpillar9@users.noreply.github.com>
2019-11-21 02:24:08 -0500

    lista-documento

---
+++ p2pfiles/src/app/lista-documento/lista.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista',
+  template: `
+    <p>
+      lista works!
+    </p>
+  `,
+  styles: []
+})
+export class ListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
